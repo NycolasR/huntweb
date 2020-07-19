7432f9608e0c8e72d2cd29cae0588b1612cb3298
NycolasR <nycolasramon3@gmail.com>
2020-07-19 19:32:46 -0300

    Navegação entre páginas

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,8 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     };
 
     componentDidMount() { // Método executado quando o componente for mostrado em tela
@@ -13,16 +15,40 @@ export default class Main extends Component {
     }
 
     // Funções nossas devem ser arrow functions
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        this.setState({ products: response.data.docs });
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     };
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        // Se estiver na primeira página, não fazer nada
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        // Se estiver na última página, não fazer nada
+        if(page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
 
     render() {
         //return <h1>Contagem de produtos: {this.state.products.length}</h1>;
 
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -31,9 +57,17 @@ export default class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="">Acessar</a>
+                        <a href="http://github.com">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>
+                        Anterior
+                    </button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+                        Próxima
+                    </button>
+                </div>
             </div>
         );
     }
