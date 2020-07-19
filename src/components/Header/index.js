99d2e9ba3e56325100d1c84aa44e0657fa5d8c0e
NycolasR <nycolasramon3@gmail.com>
2020-07-18 23:00:23 -0300

    Criação do Header e primeiras estiizações

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+// Stateless components: objetos criados com apenas funções
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
